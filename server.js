a427cc65d97ee80556927149c9e47a109521993a
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-07-28 17:04:47 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const ejs = require('ejs')
 const eel = require('express-ejs-layouts')
+const mongoose = require('mongoose')
 
 // importing data from routes/index.js
 const indexroute = require('./routes/index')
@@ -16,9 +21,25 @@ app.set('layout','layouts/layout')
 app.use(eel)
 app.use(express.static('public'))
 
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error',()=>{
+    // console.error(error);
+    console.log('kuch to hua');
+})
+
+db.once('open',()=>{
+    console.log('mongoose is running');
+})
+
 
 app.use('/',indexroute)
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT||3000,()=>{
     console.log('project is running on port 3000');
 })
